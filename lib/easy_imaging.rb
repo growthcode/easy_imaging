67360c825a7aeb865ab591996540302eb62b480b
Hethe Berg <growthcode@gmail.com>
2015-06-21 22:55:17 -0500

    rotate 1 image from including gem

--- lib/easy_imaging.rb
+++ lib/easy_imaging.rb
@@ -1,5 +1,32 @@
 require "easy_imaging/version"
+require "mini_magick"
 
 module EasyImaging
-  # Your code goes here...
+  def rotate(*args)
+    method_called = __method__
+    all_style_keys.each do |style|
+      self.process(style, method_called, *args)
+    end
+  end
+
+  def style_original
+    :original
+  end
+
+  # returns an array of model's paperclip keys, excluding the :original'
+  def style_keys
+    self.style.keys
+  end
+
+  def all_style_keys
+    self.paperclip_image.styles.keys + [style_original]
+  end
+
+  def process(style, method_called, *args)
+    path = self.paperclip_image.path(style)
+    source = MiniMagick::Image.open(path)
+    source = source.send(method_called, *args)
+    source.write(path)
+  end
 end
+
---
+++ lib/easy_imaging/engine.rb
@@ -0,0 +1,5 @@
+module EasyImaging
+  class Engine < ::Rails::Engine
+    isolate_namespace EasyImaging
+  end
+end
